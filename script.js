f63b481fdf9f1b7d1992ebd9134ca0f6dc3a6976
bob <bob@gmail.com>
2022-01-24 23:04:20 +0500

    click

--- script.js
+++ script.js
@@ -1,90 +1,115 @@
 class Scroll {
-    constructor(obj) {
-        this.element = document.querySelector(obj.selector)
+  constructor(obj) {
+    this.element = document.querySelector(obj.selector);
 
-        this.top = obj.top
+    this.top = obj.top;
 
-        this.element.style.position = 'fixed'
-        this.unit = obj.unit
+    this.element.style.position = "fixed";
+    this.unit = obj.unit;
 
-        this.element.style.top = this.scroll()
+    this.element.style.top = this.scroll();
 
-        window.addEventListener('scroll', () => this.scroll())
+    window.addEventListener("scroll", () => this.scroll());
+  }
+
+  scrollNumber() {
+    if (this.unit == "px") {
+      return this.top >= 0 ? this.top : 0;
+    } else if (this.unit == "%" || this.unit == undefined) {
+      return (window.innerHeight / 100) * this.top - this.element.clientHeight;
     }
+  }
 
-    scrollNumber() {
-        if(this.unit == 'px') {
-            return this.top >= 0 ? this.top : 0;
-        }else if(this.unit == '%' || this.unit == undefined) {
-            return (window.innerHeight / 100 * this.top) - this.element.clientHeight
-        }
+  scroll() {
+    this.window = this.scrollNumber();
+
+    if (this.window - scrollY > 0) {
+      this.element.style.top = this.window - scrollY + "px";
+    } else {
+      this.element.style.top = 0;
     }
-
-    scroll() {
-        this.window = this.scrollNumber()
-
-        if(this.window - scrollY > 0) {
-            this.element.style.top = this.window - scrollY + 'px'
-        }else {
-            this.element.style.top = 0
-        }
-    }
+  }
 }
 
-const myScroll = new Scroll(
-    {
-        selector: '.header__nav',
-        top: 100
-    }
-    )
+const myScroll = new Scroll({
+  selector: ".header__nav",
+  top: 100,
+});
 
+class Move {
+  constructor(obj) {
+    this.element = document.querySelector(obj.selector);
+    this.elementH = this.element.clientHeight;
+    this.elementW = this.element.clientWidth;
 
-class Move{
-    constructor(obj){
+    this.parent = document.querySelector(obj.parent);
+    this.parentH = this.parent.clientHeight;
+    this.parentW = this.parent.clientWidth;
 
-        this.element = document.querySelector(obj.selector);
-        this.elementH = this.element.clientHeight;
-        this.elementW = this.element.clientWidth;
+    this.boxW = document.querySelector('.header__menu-content').clientWidth 
 
-        this.parent = document.querySelector(obj.parent);
-        this.parentH = this.parent.clientHeight;
-        this.parentW = this.parent.clientWidth;
+    this.nav = document.querySelector(obj.nav)
+    this.navH = document.querySelector(obj.nav).clientHeight;
 
-        this.navH= document.querySelector(obj.nav).clientHeight
+    this.element.addEventListener("mouseover", () => {
+      this.randPostition();
+    });
+  }
+  randPostition() {
+    this.y = this.randY();
+    this.x = this.randX();
 
-        this.element.addEventListener('mouseover', ()=>{
-            this.randPostition()
-        })
-    }
-    randPostition(){
-
-        this.y = this.randY();
-        this.x = this.randX()
-
-
-        this.element.style =`
+    this.element.style = `
         left:${this.x}px;
         top:${this.y}px;
-        
-        `
-        console.log(this.y);
-        console.log(this.x);
-        console.log(this.navH);
-        console.log(this.parentH);
-        console.log(this.parentW);
-        console.log(this.elementW);
-    }
+        `;
+  }
 
-    randY(){
-        return Math.floor(Math.random()*((this.parentH-this.navH-this.elementH)+1))
-    }
-    randX(){
-        return Math.floor(Math.random()*((this.parentW - this.elementW)+1))
-    }
+  randY() {
+    return Math.floor(
+      Math.random() * (this.parentH - this.navH - this.elementH + 1)
+    );
+  }
+  randX() {
+      if(this.nav.className == 'active'){
+          return Math.floor(Math.random() * (this.parentW - this.elementW + 1));
+      }else{
+           return Math.floor(Math.random() * ((this.parentW) -this.elementW  -  this.boxW + 1)) +  this.boxW ;
+      }
+    this.boxH
+  }
 }
 
 const run = new Move({
-    selector:'.header__content',
-    parent:'.header',
-    nav:".header__nav"
-})
+  selector: ".header__content",
+  parent: ".header",
+  nav: ".header__nav",
+});
+
+class Click {
+  constructor(obj) {
+    this.button = document.querySelectorAll(obj.btn);
+
+    this.button.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        this.box = document.querySelector(obj.box);
+
+        this.nav = document.querySelector(obj.nav);
+
+        this.nav.classList.toggle("active");
+        this.nav.classList.add("transition");
+        setTimeout(()=>{
+            this.nav.classList.remove("transition");
+        },500)
+
+        this.box.classList.toggle("active");
+
+      });
+    });
+  }
+}
+const click = new Click({
+  btn: ".btn",
+  box: ".header__menu-content",
+  nav: ".header__nav",
+});
